21182244731293040acf0383bc1128624c2eeb2d
seperry83 <seperry83@gmail.com>
2021-08-30 12:47:41 -0700

    process SF wq data

---
+++ Water_Quality/wq_funcs.R
@@ -0,0 +1,15 @@
+# Functions for WQ data
+# Authors: Dave Bosworth, Sarah Perry
+
+# read in absolute fp
+drought_abs_path <- function(fp_rel = NULL) {
+  fp_fastr <- 'California Department of Water Resources/Drought Synthesis - Documents/WQ_Team/'
+  
+  if (is.null(fp_rel)) {
+    fp_abs <- normalizePath(file.path(Sys.getenv('USERPROFILE'), fp_fastr))
+  } else {
+    fp_abs <- normalizePath(file.path(Sys.getenv('USERPROFILE'), fp_fastr, fp_rel))
+  }
+  
+  return(fp_abs)
+}
