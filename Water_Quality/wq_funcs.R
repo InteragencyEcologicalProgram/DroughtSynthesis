467c853440a3ee48beac91388ca56a7661aa408e
seperry83 <seperry83@gmail.com>
2021-08-30 14:25:11 -0700

    added season func

--- Water_Quality/wq_funcs.R
+++ Water_Quality/wq_funcs.R
@@ -1,6 +1,8 @@
 # Functions for WQ data
 # Authors: Dave Bosworth, Sarah Perry
 
+#' @importFrom magrittr %>%
+
 # read in absolute fp
 drought_abs_path <- function(fp_rel = NULL) {
   fp_fastr <- 'California Department of Water Resources/Drought Synthesis - Documents/'
@@ -12,4 +14,22 @@ drought_abs_path <- function(fp_rel = NULL) {
   }
   
   return(fp_abs)
+}
+
+add_season_col <- function(df) {
+  assertthat::assert_that(
+    assertthat::is.date(df$Date),
+    msg = "add_season_col function\n'Date' variable must be in the date data type."
+  )
+  
+  df <- df %>%
+  dplyr::mutate(
+    Season =
+      case_when(
+        month(Date) %in% c(12,1,2) ~ 'Winter',
+        month(Date) %in% c(3,4,5) ~ 'Spring',
+        month(Date) %in% c(6,7,8) ~ 'Summer',
+        month(Date) %in% c(9,10,11) ~ 'Fall'
+      )
+  )
 }
