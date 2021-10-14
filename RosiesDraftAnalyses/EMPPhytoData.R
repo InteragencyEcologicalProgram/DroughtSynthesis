c70647a1eda8d158c3caac874fdb61cdc2149e18
rosehartman <rosehartman@gmail.com>
2021-10-14 15:29:50 -0700

    Added more visual index data

--- RosiesDraftAnalyses/EMPPhytoData.R
+++ RosiesDraftAnalyses/EMPPhytoData.R
@@ -7,16 +7,15 @@ library(readxl)
 
 
 #Import Tiffany's files
-Phy2011 = read_excel("data/2011 HAB Data.xlsx")
-Phy2012 = read_excel("data/2012 HAB Data.xlsx")
-Phy2013 = read_excel("data/2013 HAB Data.xlsx")
-Phy2014 = read_excel("data/2014 HAB Data.xlsx")
-Phy2015 = read_excel("data/2015 HAB Data.xlsx")
-Phy2016 = read_excel("data/2016 HAB Data.xlsx")
-Phy2017 = read_excel("data/2017 HAB Data.xlsx")
-Phy2018 = read_excel("data/2018 HAB Data.xlsx")
-Phy2019 = read_excel("data/2019 HAB Data.xlsx")
-
+Phy2011 = read_excel("data/HABs/2011 HAB Data.xlsx")
+Phy2012 = read_excel("data/HABs/2012 HAB Data.xlsx")
+Phy2013 = read_excel("data/HABs/2013 HAB Data.xlsx")
+Phy2014 = read_excel("data/HABs/2014 HAB Data.xlsx")
+Phy2015 = read_excel("data/HABs/2015 HAB Data.xlsx")
+Phy2016 = read_excel("data/HABs/2016 HAB Data.xlsx")
+Phy2017 = read_excel("data/HABs/2017 HAB Data.xlsx")
+Phy2018 = read_excel("data/HABs/2018 HAB Data.xlsx")
+Phy2019 = read_excel("data/HABs/2019 HAB Data.xlsx")
 #Check to see that they read in correctly:
 str(Phy2011)
 #looks pretty good
@@ -78,9 +77,38 @@ str(PhytoAll)
 
 summary(PhytoAll)
 
+
+#now let's add the other sample names
+
+Phy2011b = read_excel("data/HABs/All 2011 Data.xlsx") 
+Phy2012b = read_excel("data/HABs/All 2012 Data.xlsx")
+Phy2013b = read_excel("data/HABs/All 2013 Data.xlsx") 
+Phy2014b = read_excel("data/HABs/All 2014 Data.xlsx")
+Phy2015b = read_excel("data/HABs/All 2015 Data.xlsx")
+Phy2016b = read_excel("data/HABs/All 2016 Data.xlsx")
+Phy2017b = read_excel("data/HABs/All 2017 Data.xlsx")
+Phy2018b = read_excel("data/HABs/All 2018 Data.xlsx")
+Phy2019b = read_excel("data/HABs/All 2019 Data.xlsx")
+Phy2020b = read_excel("data/HABs/All 2020 Data.xlsx")
+
+Phyallb = bind_rows(Phy2011b, Phy2013b, Phy2014b, Phy2015b, Phy2016b, Phy2017b, Phy2018b, Phy2019b, Phy2020b)
+
+Phyallsamples = group_by(Phyallb, SampleDate, StationCode) %>%
+  summarize(N = n())
+
+
+Phyallc = left_join(Phyallsamples, PhytoAll) %>%
+  mutate(Genus = case_when(
+    is.na(Genus) ~ "NoHABs",
+    TRUE~Genus
+  ),
+  CarbonperML = case_when(
+    is.na(CarbonperML) ~ 0,
+    TRUE ~ CarbonperML))
+
 #now we can add in the zeros and stuff. 
 
-Phyto0 = complete(PhytoAll, SampleDate, StationCode, Taxon, 
+Phyto0 = complete(Phyallc, SampleDate, StationCode, Taxon, 
                   fill = list(UnitAbundance = 0, `Organisms per mL (Unit Abundance per mL)`=0,
                               `Average Biovolume per mL (cubic microns per mL)`=0,
                               CarbonperML = 0))
@@ -90,7 +118,7 @@ unique(PhytoAll$Taxon)[order(unique(PhytoAll$Taxon))]
 unique(PhytoAll$Genus)
 #That's better. We're going to group by genus
 
-PhytoAll2 = group_by(PhytoAll, SampleDate, StationCode, Genus) %>%
+PhytoAll2 = group_by(Phyallc, SampleDate, StationCode, Genus) %>%
   summarize(Abundance = sum(`Unit Abundance`), 
             OrgPerML = sum(`Organisms per mL (Unit Abundance per mL)`),
             BioVolperML = sum(`Average Biovolume per mL (cubic microns per mL)`),
@@ -98,22 +126,95 @@ PhytoAll2 = group_by(PhytoAll, SampleDate, StationCode, Genus) %>%
 
 Phyto0 = pivot_wider(PhytoAll2, id_cols = c(SampleDate, StationCode), 
                      names_from = Genus, values_from = CarbonperML, values_fill = 0) %>%
-  pivot_longer(cols = 3:12,names_to = "Genus", values_to = "CarbonperML")
+  pivot_longer(cols = 3:13,names_to = "Genus", values_to = "CarbonperML") %>%
+  filter(Genus != "NoHABs")
+
+
+
+###############################################################################
+#some of the station codes don't line up with the wq data
+Phyto0 = mutate(Phyto0, StationCode = case_when(
+  StationCode == "EZ2 SJR" ~ "EZ2-SJR",
+  StationCode == "EZ2SJR" ~ "EZ2-SJR",
+  StationCode == "EZ6 SJR" ~ "EZ6-SJR",
+  StationCode == "EZ6SJR" ~ "EZ6-SJR",
+  StationCode == "EZ6SAC" ~ "EZ6",
+  StationCode == "EZ2SAC" ~ "EZ2",
+  StationCode == "EZ2 SAC" ~ "EZ2",
+  StationCode == "EZ6 SAC" ~ "EZ6",
+  StationCode == "EZ6 SAC" ~ "EZ6",
+  StationCode == "D16-Twitchell" ~ "D16",
+  StationCode == "D16-Twitchel" ~ "D16",
+  StationCode == "D16 - Twitchel" ~ "D16",
+  StationCode == "D16 - Twitchell" ~ "D16",
+  StationCode == "D16- Twitchel" ~ "D16",
+  StationCode == "D16 Twitchell" ~ "D16",
+  StationCode == "C3A- Hood" ~ "C3A",
+  StationCode == "C3A-Hood" ~ "C3A",
+  StationCode == "C3A Hood" ~ "C3A",
+  StationCode == "NZ542" ~ "NZS42",
+  StationCode == "E26" ~ "EZ6",
+  StationCode == "E22" ~ "EZ2",
+  TRUE ~ StationCode
+),
+Month = month(SampleDate), Year = year(SampleDate))
+
 
 ###################################################################################
 #now let's add the water quality data
 
 WQ = read_csv("data/SACSJ_delta_water_quality_1975_2020.csv")
-WQ = mutate(WQ, Date = mdy(Date)) %>%
-  rename(StationCode = Station, SampleDate = Date)
+WQ = mutate(WQ, Date = mdy(Date), Month = month(Date), Year = year(Date)) %>%
+  rename(StationCode = Station, SampleDate = NULL)
 
+
+#join the data together
 Phyto02 = left_join(Phyto0, WQ)
 test = filter(Phyto02, is.na(WTSurface))
-missing = group_by(test, SampleDate, StationCode) %>%
+
+#how many samples don't have paired water quality?
+missing = group_by(test, Month, Year, StationCode) %>%
   summarize(N = n())
 
-testagain = filter(WQ, StationCode == "D28A" & year(SampleDate)== 2011)
-write.csv(missing, "MissingSamples.csv")
-write.csv(PhytoAll2, "PhytoSamples.csv")
+#let's try attaching the gps coordinates
+GPS = read_csv("data/EMP_Discrete_Water_Quality_Stations_1975-2020.csv") %>%
+  rename(StationCode = Station) %>%
+  select(StationCode, Latitude, Longitude)
+Phyto02reg = left_join(Phyto02, GPS) %>%
+  mutate(Latitude = case_when(
+    is.na(Latitude) ~ NorthLat,
+    TRUE ~ Latitude
+  ), Longitude = case_when(
+    is.na(Longitude) ~ WestLong,
+    TRUE ~ Longitude
+  )) %>%
+  rename(PhytoDate = SampleDate, WQDate = Date) %>%
+  select(PhytoDate, WQDate, StationCode, Month, Year, Latitude, Longitude, Genus, CarbonperML,
+         AirTemp, Chla, Pheophytin, TotAmmonia, DissAmmonia, DissNitrateNitrite, DOC,
+         TOC, DON, TON, DissOrthophos, TotPhos, DissSilica, TDS, TSS, TKN, Depth, Secchi, Microcystis,
+         SpCndSurface, DOSurface, WTSurface) %>%
+  filter(Year < 2020)
+
+write.csv(Phyto02reg, "HABBiomassByGenus.csv")
+
+#calculate total biomass of harmful algal species
+phytosum = group_by(Phyto02reg, PhytoDate, WQDate, StationCode, Month, Year, Latitude, Longitude, 
+                    AirTemp, Chla, Pheophytin, TotAmmonia, DissAmmonia, DissNitrateNitrite, DOC,
+                    TOC, DON, TON, DissOrthophos, TotPhos, DissSilica, TDS, TSS, TKN, Depth, Secchi, Microcystis,
+                    SpCndSurface, DOSurface, WTSurface) %>%
+  summarize(CarbonperML = sum(CarbonperML))
+
+write.csv(phytosum, "HABBiomassTotal.csv")
+
+Microcystis = filter(Phyto02reg, Genus == "Microcystis")
+
+write.csv(Microcystis, "MicrocystisBiomass.csv")
+
 
 ####################################################################################
+phytosum2 = group_by(Phyto02reg, Month, Year) %>%
+  summarize(CarbonperMLm = mean(CarbonperML), sd = sd(CarbonperML))
+
+ggplot(phytosum2, aes(x = Month, y = CarbonperMLm)) +geom_col() + facet_wrap(~Year)
+
+
--- RosiesDraftAnalyses/HABdiscrete.R
+++ RosiesDraftAnalyses/HABdiscrete.R
@@ -4,22 +4,29 @@ library(tidyverse)
 library(discretewq)
 library(sf)
 library(emmeans)
+library(lubridate)
+library(ggmap)
 
-HABs = wq( Start_year = 2010,End_year = 2020,
-  Sources = c("EMP", "STN", "FMWT", "EDSM", "DJFMP", "20mm", 
-              "Baystudy", "USGS"))
 
+HABs = read_csv("data/HABs/WQ_data_integrated4.csv")
 str(HABs)
+HABs = mutate(HABs,  Datex = as.Date(Date2, origin = "1899-12-30"),
+              MonthYear = NULL, Datetime = NULL, Month = month(Datex), Year = year(Datex))
+
 
 #load regions shapefile
 regions = st_read("C:/Users/rhartman/OneDrive - California Department of Water Resources/Drought/Barrier/BarrierRegions/shpExport.shp") %>%
   st_make_valid()
 
 
-HABssf = filter(HABs, !is.na(Longitude)) %>%
+HABssf = filter(HABs, !is.na(Longitude), !is.na(Latitude)) %>%
   st_as_sf(coords = c("Longitude", "Latitude"), crs = st_crs(4326))
 
-ggplot() + geom_sf(data = regions) + geom_sf(data = HABssf)
+foo = filter(HABssf, Source == "STN", Year == 2021)
+
+ggplot() + geom_sf(data = WW_Delta) + geom_sf(data = HABssf)+
+  geom_sf_label(data = HABssf, aes(label = Station), 
+                position = "jitter", label.size = 0.05)
 #crop it to the area right around the barrier
 BarHABs = st_crop(HABssf, regions)
 
@@ -30,13 +37,13 @@ BH =   st_join(BarHABs, regions) %>%
   filter(!is.na(Regions), Month >4) %>%
   mutate(Yearf = as.factor(Year))
 
-BHm = filter(BH, !is.na(Microcystis))
+BHm = filter(BH, !is.na(Microcystis), Year >2013, Month %in% c(5,6,7,8,9, 10))
 
 ggplot(BHm, aes(x = as.factor(Year), y = Microcystis)) +geom_boxplot()+ facet_wrap(~Regions)
 
 ggplot(BHm, aes(x = Regions, y = Microcystis)) +geom_boxplot()+ facet_wrap(~Year)
 
-ggplot(BH, aes(x = Date, y = Temperature)) +geom_boxplot()+ facet_wrap(~Year)
+ggplot(BH, aes(x = Date, y = Temperature)) +geom_point()+ facet_wrap(~Year)
 
 
 ggplot(BHm, aes(x = Regions, fill = as.factor(Microcystis))) +geom_bar(position = "fill")+ facet_wrap(~Year) +
@@ -45,6 +52,8 @@ ggplot(BHm, aes(x = Regions, fill = as.factor(Microcystis))) +geom_bar(position 
                     name = "Microcystis")+ ylab("Relative Frequency")
 
 #definitely more microcystis reported in 2016 and 2018 than 2015
+
+
 
 BH2015.2014 = filter(BHm, Year %in% c(2014, 2015, 2016))
 BH2018 = filter(BHm, Year == 2018)
@@ -75,10 +84,11 @@ ggplot(BH2018, aes(x = Month, fill = as.factor(Microcystis))) +geom_bar(position
 #OK, how do we model this? Maybe converte to presence/absence?
 BHm = mutate(BHm, HABPA = case_when(
   Microcystis == 1 ~ FALSE,
-  Microcystis > 1 ~ TRUE))
+  Microcystis > 1 ~ TRUE)) %>%
+filter(Year>2013)
 
 #now a glm (binomial)
-bn1 = glm(HABPA ~ Month + Yearf+Regions, data = BH, family = "binomial")
+bn1 = glm(HABPA ~ Month + Temperature + Yearf*Regions, data = BHm, family = "binomial")
 summary(bn1)
 emmeans(bn1, pairwise ~ Yearf)
 plot(bn1)
@@ -94,6 +104,159 @@ table(emp$Month)
 
 #Look at potential drivers of the bloom
 library(lme4)
-mm1 = glmer(HABPA ~ scale(Temperature) + scale(Secchi) +Month+ (1|Yearf) + (1|Station), data = BHm, family = "binomial")
+dat = filter(BHm, Year > 2013)
+mm1 = glmer(HABPA ~ scale(Temperature) + scale(Secchi) +Month+ (1|Yearf) + (1|Station), data = dat, family = "binomial")
 summary(mm1)
 visreg(mm1)
+
+
+
+#############################################################################################
+#What do I use for the SFHSR?
+
+library(deltamapr)
+HABs2021 = filter(HABs, Year == 2021)
+sumfall = filter(HABs, Month %in% c(6,7,8,9,10))
+ggplot(sumfall, aes(x = Year, fill = as.factor(Microcystis))) +geom_bar(position = "fill")+ 
+  scale_fill_manual(values = c("white", "tan2", "yellow", "red", "darkred"), 
+                    labels = c("absent", "low", "medium", "high", "very high"),
+                    name = "Microcystis")+ ylab("Relative Frequency")
+
+reg2 = R_EDSM_Regions_1819P1 %>%
+  st_transform(crs = st_crs(4326))
+
+reg3 = R_EDSM_Strata_1718P1%>%
+  st_transform(crs = st_crs(4326))
+
+
+HABssf1 = filter(sumfall, !is.na(Longitude), !is.na(Latitude)) %>%
+  st_as_sf(coords = c("Longitude", "Latitude"), crs = st_crs(4326))
+
+ggplot() + geom_sf(data = reg3) + geom_sf(data = HABssf1)
+#crop it to the area we are interested in
+sfhab = st_crop(HABssf1, reg3)
+
+#plot the map
+ggplot() + geom_sf(data = WW_Delta, fill = "lightgrey")+
+  geom_sf(data = reg3, aes(fill = Stratum), alpha = 0.4) + 
+  geom_sf(data = sfhab, aes(shape = Source)) +
+  scale_fill_brewer(palette = "Set3", guide = NULL)+
+  #coord_sf(xlim = c(-122.3, -121.4), ylim = c(37.8, 38.6))+
+  geom_sf_label(data = reg3, aes(label = Stratum), 
+                position = "jitter", label.size = 0.05)+
+  theme_bw()
+
+SFH =   st_join(sfhab, reg3) %>%
+  st_drop_geometry() %>%
+  filter(!is.na(Stratum), !is.na(Microcystis)) %>%
+  mutate(Yearf = as.factor(Year),
+         Month2 = factor(Month, levels = c(6,7,8,9,10),
+                         labels = c("Jun", "Jul", "Aug", "Sep", "Oct")),
+         Stratum = factor(Stratum, levels = c("Western Delta", "Suisun Bay", "Suisun Marsh", "Lower Sacramento",
+                                              "Lower San Joaquin", "Eastern Delta", "Southern Delta",
+                                              "Cache Slough/Liberty Island", "Sac Deep Water Shipping Channel",
+                                              "Upper Sacramento"), 
+                          labels = c("Far West", "Suisun Bay", "Suisun Marsh", "Lower Sac",
+                                     "Lower SJ", "East Delta", "South Delta", "Cache/Liberty", "SDWSC",
+                                     "Upper Sac")))        
+
+ggplot(SFH, aes(x = Stratum, fill = as.factor(Microcystis))) +geom_bar(position = "fill")+ facet_wrap(~Year)+
+  scale_fill_manual(values = c("white", "tan2", "yellow", "red", "darkred"), 
+                    labels = c("absent", "low", "medium", "high", "very high"),
+                    name = "Microcystis")+ ylab("Relative Frequency") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+
+
+ggplot(filter(SFH, Year == 2021), aes(x = Stratum, fill = as.factor(Microcystis))) + 
+  geom_bar(position = "fill")+ 
+  facet_grid(.~Month2)+
+  scale_fill_manual(values = c("white", "tan2", "yellow", "red", "darkred"), 
+                    labels = c("absent", "low", "medium", "high", "very high"),
+                    name = "Microcystis")+ ylab("Relative Frequency")+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1))
+
+
+ggplot(SFH, aes(x = Year, fill = as.factor(Microcystis))) +geom_bar(position = "fill")+ 
+  scale_fill_manual(values = c("white", "tan2", "yellow", "red", "darkred"), 
+                    labels = c("absent", "low", "medium", "high", "very high"),
+                    name = "Microcystis")+ ylab("Relative Frequency") 
+
+ggplot(filter(SFH, Year == 2021), aes(x = Month, fill = as.factor(Microcystis))) + 
+  geom_bar(position = "fill")+ 
+  #facet_grid(.~Month2)+
+  scale_fill_manual(values = c("white", "tan2", "yellow", "red", "darkred"), 
+                    labels = c("absent", "low", "medium", "high", "very high"),
+                    name = "Microcystis")+ ylab("Relative Frequency")+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1))
+
+###################################################################################
+#box plots by stratum
+library(wql)
+
+SFH2021 = filter(SFH, Year == 2021) %>%
+  mutate(Salinity = ec2pss(Conductivity/1000, t = Temperature))
+
+
+ggplot(SFH2021, aes(x = Stratum, y = Temperature, fill = Stratum)) + 
+  geom_boxplot()+ 
+  facet_grid(.~Month2)+ ylab("Water Temperature C")+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1))
+
+ggplot(SFH2021, aes(x = Stratum, y = Turbidity)) + 
+  geom_boxplot()+ 
+  facet_grid(.~Month2)+ ylab("Turbidity (FNU)")+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1))
+
+ggplot(SFH2021, aes(x = Stratum, y = Conductivity)) + 
+  geom_boxplot()+ 
+  facet_grid(.~Month2)+ ylab("Conductivity uS/cm")+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1))
+
+
+
+ggplot(SFH2021, aes(x = Stratum, y = Salinity)) + 
+  geom_boxplot()+ 
+  facet_grid(.~Month2)+ ylab("Salinity (PSU)")+
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 1))
+
+####################################################################################
+#Models
+library(readxl)
+SFH2 = mutate(SFH, HABPA = case_when(
+  Microcystis == 1 ~ FALSE,
+  Microcystis > 1 ~ TRUE)) %>%
+  filter(Year >2013)
+
+#year types
+yeartypes = read_excel("data/Integrated data set.xlsx", sheet = "yearassignments")
+
+SFHwflow = left_join(SFH2, yeartypes)
+#now a glm (binomial)
+
+bn1 = glm(HABPA ~ Month + Yearf+Stratum, data = SFH2, family = "binomial")
+summary(bn1)
+emmeans(bn1, pairwise ~ Yearf)
+plot(bn1)
+
+library(visreg)
+library(lme4)
+library(lmerTest)
+visreg(bn1, scale = "response")
+visreg(bn1, xvar = "Regions", by = "Yearf")
+
+
+Ind1 = glmer(HABPA ~ Month + Index +Salinity + Temperature+ (1|Yearf), data = SFHwflow, family = "binomial")
+summary(bn1)
+plot(bn1)
+visreg(bn1)
+library(visreg)
+
+#Could I use the effect of the TUCP on water year index to figure out how big of an impact
+#it might have had on weeds?
+
+flow1= glmer(HABPA ~ Month + ShortTerm +Salinity + Temperature+ (1|Yearf), data = SFHwflow, family = "binomial")
+summary(flow1)
+emmeans(flow1, pairwise ~ ShortTerm)
+plot(flow1)
+visreg(bn1)
+library(visreg)
