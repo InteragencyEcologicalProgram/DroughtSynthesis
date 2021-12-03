965db4b27b74b7623a01ada9febb66197fd26400
keithbg <kbouma-gregson@usgs.gov>
2021-12-03 09:10:38 -0800

    clean up script and add CSV export

--- PrimaryProducerTeam/Scripts/Analysis_chla.R
+++ PrimaryProducerTeam/Scripts/Analysis_chla.R
@@ -5,12 +5,16 @@ library(tidyverse)
 library(lme4)
 library(lmerTest)
 library(emmeans)
-
 source("Scripts/ggplot_themes.R")
+
+## Load data frames from Data_format.R
+load("Data/DS_dataframes.Rdata")
 
 
 #### DATA FILTERING ####
 filter_chla_data <- function(data, min_samps_yr, min_yrs, excluded_regions, seasons, min_result){
+  require(tidyverse)
+  require(sf)
 
 chla_station_summary <- data %>%
   group_by(Source, Station, year) %>%
@@ -41,7 +45,7 @@ chla_stations_filt2 <- data_filt %>%
   summarize(count_chla= length(chla)) %>% 
   group_by(Source, Station, SourceStation) %>% 
   mutate(mean_counts= mean(count_chla)) %>% 
-  filter(mean_counts > 6) #%>% # Average amount of data per year
+  filter(mean_counts > 6) %>% # Average amount of data per year
   select(SourceStation) %>% 
   distinct(.)
   
@@ -51,7 +55,7 @@ data_filt2 <- data_filt %>%
 
 ## Unique station list with Lat/Longs
 stations_filt.sf <- data_filt2 %>% 
-  select(Source, Station, Latitude, Longitude) %>% 
+  select(Source, Station, Region, Latitude, Longitude) %>% 
   distinct(.) %>%
   sf::st_as_sf(., coords= c("Longitude", "Latitude"), crs= 4269) %>% #NAD83
   sf::st_transform(., crs= 26910) %>% # NAD 83/ UTM10N
@@ -61,84 +65,88 @@ return(list(data= data_filt2, stations= stations_filt.sf))
 
 }
 
-## # load data frames from Data_format.R
-load("Data/DS_dataframes.Rdata")
-
 
 ## Filter data based on
 # Minimum samples per year per site
+# Minimum number of years sampled at a site
 # Regions to exclude from the analysis
 # Seasons to include in the analysis
 # Minimum result threshold
 chla_data_filt_list <- filter_chla_data(data= DS_data,
-                                 min_samps_yr = 12,
-                                 min_yrs= 6,
-                                 excluded_regions = c("Far West"),
-                                 seasons= c("Summer", "Fall", "Spring", "Winter"),
-                                 min_result = 0)
+                                        min_samps_yr = 12,
+                                        min_yrs= 6,
+                                        excluded_regions = c("Far West"),
+                                        seasons= c("Summer", "Fall", "Spring", "Winter"),
+                                        min_result = 0)
 
 
-chla_data_filt <- mc_data_filt_list$data
-chla_stations_filt.sf <- mc_data_filt_list$stations
+chla_data_filt <- chla_data_filt_list$data
+chla_stations_filt.sf <- chla_data_filt_list$stations
 
-Station_samples <- DS_data_filt %>% 
-  count(Source, Station)
 
 ## Get data frame of unique stations in the filtered data set
-chla_stations_filt.sf <- chla_stations.sf %>%
-  filter(Station %in% unique(DS_data_filt$Station)) %>% # Remove Suisun Marsh because only have 2-3 EMP stations in that Region
-  distinct(.)
+#chla_stations_filt.sf <- chla_stations.sf %>%
+#  filter(Station %in% unique(DS_data_filt$Station)) %>% # Remove Suisun Marsh because only have 2-3 EMP stations in that Region
+#  distinct(.)
 
 
 ## Transform chla data in preparation for statistical analyses
 ## Average all chla at each station by month, log transform chla
-DS_data_stats <- DS_data_filt %>%
+chla_data_stats <- chla_data_filt %>%
   group_by(ds_year, month, Source, Station, Region, SubRegion, ds_year_type, Date, Season) %>%
-  summarize(chlaAvg= mean(chla, na.rm= TRUE),
-            count= n()) %>%
-  mutate(chla_log10= log10(chlaAvg), #log10
-         chla_log= log(chlaAvg), # natural log
+  summarize(samples_in_month= n(),
+            chlaAvg= mean(chla, na.rm= TRUE),
+            chlaAvg_log10= log10(chlaAvg), #log10
+            chlaAvg_log= log(chlaAvg), # natural log
+            ) #%>%
+  mutate(chlaAvg_log10= log10(chlaAvg), #log10
+         chlaAvg_log= log(chlaAvg), # natural log
          month_pair= ifelse(month == 6 | month == 7, "1J-J",
                             ifelse(month == 8 | month == 9, "2A-S", "3O-N"))) %>%
   ungroup()
 
+## WRITE CSV FILE
+chla_data_stats %>% 
+  select(Date, everything(), -chlaAvg_log) %>% 
+   write_csv(., "Data/chla_data_stats.csv")
+
 
 ## Summary Tables of data
-source_summary <- DS_data_stats %>%
+source_summary <- chla_data_stats %>%
   group_by(Source) %>%
   count(Source)
 sum(source_summary$n)
 
-SubRegion_month_summary <- DS_data_stats %>%
+SubRegion_month_summary <- chla_data_stats %>%
   group_by(Source, ds_year, month, Region, Season) %>%
   count(SubRegion)
 
-Station_month_summary <- DS_data_stats %>%
+Station_month_summary <- chla_data_stats %>%
   group_by(Source, ds_year, month, Region, SubRegion, Season) %>%
   count(Station)
 sum(Station_month_summary$n)
 
-year_summary <- DS_data_stats %>%
+year_summary <- chla_data_stats %>%
   group_by(Source, ds_year, Region) %>%
   count(Region)
 
 sum(year_summary$n)
 
-Station_count <- DS_data_stats %>%
+Station_count <- chla_data_stats %>%
   select(Source, Station) %>% 
   distinct(.) %>% 
   group_by(Source) %>%
   count(Source)
 sum(Station_count$n)
 
-Station_Region_count <- DS_data_stats %>%
+Station_Region_count <- chla_data_stats %>%
   select(Source, Region, Station) %>% 
   distinct(.) %>% 
   group_by(Region) %>%
   count(Region)
 sum(Station_count$n)
 
-Year_type_summary <- DS_data_stats %>%
+Year_type_summary <- chla_data_stats %>%
   group_by(Source, month, Region, SubRegion) %>%
   count(ds_year_type)
 
@@ -151,15 +159,15 @@ ggplot(year_summary, aes(x= ds_year, y= n)) +
   labs(x= "Station-months per year", y= "Count") +
   scale_y_continuous(expand= c(0, 0), breaks= seq(0, 600, by= 100), labels= c("0", "", "200", "", "400", "", "600")) +
   theme_doc
-ggsave(last_plot(), filename= "year_sample_summary.png", width= 8, height= 6, dpi= 300,
-       path= "Figures")
+#ggsave(last_plot(), filename= "year_sample_summary.png", width= 8, height= 6, dpi= 300,
+#       path= "Figures")
 
 
 ## Boxplots of data
 season.colors <- c("burlywood4", "darkslategray3", "chartreuse3", "sienna3")
 
 
-ggplot(DS_data_stats, aes(x= ds_year_type, y= chla_log10)) +
+ggplot(chla_data_stats, aes(x= ds_year_type, y= chla_log10)) +
   geom_boxplot(aes(fill= Season)) +
   labs(x= "Year type", y= expression(paste("Chlorophyll-a (", mu, "g/L)"))) +
   scale_y_continuous(breaks= c(-2, -1, 0, 1, 2),
@@ -178,7 +186,7 @@ ggsave(last_plot(), filename= "chla_filtered_Season_log10.png", width= 6.5, heig
 
 
 
-ggplot(DS_data_stats, aes(x= as.factor(month), y= chla_log10)) +
+ggplot(chla_data_stats, aes(x= as.factor(month), y= chla_log10)) +
   geom_boxplot(aes(fill= ds_year_type)) +
   labs(x= "Month", y= expression(paste("Chla (", mu, "g/L)"))) +
   scale_y_continuous(breaks= c(-2, -1, 0, 1, 2),
@@ -214,16 +222,16 @@ ggsave(last_plot(), filename= "station_map_chla_filtered.png", width= 10, height
 
 #### STATISTICS ####
 fit_log10.1 <- lmer(chla_log10 ~ ds_year_type + Season + Region + (1|Station),
-                   data= DS_data_stats)
+                   data= chla_data_stats)
 summary(fit_log10.1)
 plot(fit_log10.1)
 anova(fit_log10.1)
 
-emm_year <- emmeans(fit_log10.1, specs= "ds_year_type", pbkrtest.limit = nrow(DS_data_stats))
+emm_year <- emmeans(fit_log10.1, specs= "ds_year_type", pbkrtest.limit = nrow(chla_data_stats))
 pairs(emm_year)
-emm_Region <- emmeans(fit_log10.1, specs= "Region", pbkrtest.limit = nrow(DS_data_stats))
+emm_Region <- emmeans(fit_log10.1, specs= "Region", pbkrtest.limit = nrow(chla_data_stats))
 pairs(emm_Region)
-emm_season <- emmeans(fit_log10.1, specs= "Season", pbkrtest.limit = nrow(DS_data_stats))
+emm_season <- emmeans(fit_log10.1, specs= "Season", pbkrtest.limit = nrow(chla_data_stats))
 pairs(emm_season)
 
 
