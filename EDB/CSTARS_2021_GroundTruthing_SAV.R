50a85869ddc62c03c98b3dd546ebcb3c19353596
Rasmussen <Nicholas.Rasmussen@water.ca.gov>
2021-12-14 17:20:04 -0800

    got shape files from CSTARS for Franks Tract, Big Break, and Clifton Court. Used them to filter the ground truthing data set to just the relevant samples and create sampling maps

--- EDB/CSTARS_2021_GroundTruthing_SAV.R
+++ EDB/CSTARS_2021_GroundTruthing_SAV.R
@@ -27,8 +27,11 @@ sharepoint_path_read <- normalizePath(
 cstars <- read_csv(file = paste0(sharepoint_path_read,"./CSTARS_2021_formatted.csv"))
 glimpse(cstars) #looks good. just need to make lat/long into geometry
 
-#read in shape file with Franks Tract (and Mildred Island)
-sf_franks_mildred <- read_sf("EDB/Spatial_data/Franks_Mildred.shp")
+#read in shape files for Franks Tract, Big Break, and Clifton Court
+#files from the CSTARS lab
+sf_franks <- read_sf("EDB/Spatial_data_weeds/FranksTractarea_wgs84.shp")
+sf_bbreak <- read_sf("EDB/Spatial_data_weeds/BigBreak_wgs84.shp")
+sf_ccourt <- read_sf("EDB/Spatial_data_weeds/Cliftoncourtarea_wgs84.shp")
 
 #format data set-----------
 
@@ -119,22 +122,9 @@ WW_Delta_4326 <- st_transform(WW_Delta, crs = 4326)
 
 #create subsets of data sets by site---------
 
-#Look at CRS for Dave's shape file for Franks Tract/Mildred Island 
-st_crs(sf_franks_mildred) 
-#already in the right CRS (WGS84), which is EPSG 4326
-
-#plot the shape files
-(map_ft_md <- ggplot()+
-    geom_sf(data= sf_franks_mildred, fill= "skyblue3", color= "black") 
-  )
-
-#Drop Mildred Island because I only need Franks Tract
-sf_franks <- sf_franks_mildred %>% 
-  filter(HNAME=="Franks Tract")
-
 # Create a bounding box of the Franks shapefile which will be used to crop the field data
 # and extend it by 5% on the west and north sides
-bbox_fr <- st_bbox(sf_franks)
+bbox_fr <- st_bbox(sf_franks_d)
 bbox_fr_xrange <- bbox_fr$xmax - bbox_fr$xmin
 bbox_fr_yrange <- bbox_fr$ymax - bbox_fr$ymin
 bbox_fr[1] <- bbox_fr[1] - (bbox_fr_xrange * 0.05)
@@ -142,18 +132,51 @@ bbox_fr[1] <- bbox_fr[1] - (bbox_fr_xrange * 0.05)
 #bbox_fr[2] <- bbox_fr[2] - (bbox_fr_yrange * 0.05)
 bbox_fr[4] <- bbox_fr[4] + (bbox_fr_yrange * 0.05)
 
-#Filter CSTARS data set to just those within the Franks Tract polygon
-cstars_franks <- cstars_format %>% 
-  st_filter(sf_franks) 
+#Look at CRS for CSTARS shape file forS Franks Tract, Big Break, and Clifton Court
+st_crs(sf_franks) 
+sf_franks_4326 <- st_transform(sf_franks, crs = 4326)
 
-  #create map showing Franks Tract SAV data points
-#this shape file excludes a few field points I'd like to include
-#particularly in the west edge
+st_crs(sf_bbreak) 
+sf_bbreak_4326 <- st_transform(sf_bbreak, crs = 4326)
+
+st_crs(sf_ccourt) 
+sf_ccourt_4326 <- st_transform(sf_ccourt, crs = 4326)
+
+#plot the shape files
+(map_ft <- ggplot()+
+    geom_sf(data= sf_franks_4326, fill= "skyblue3", color= "black") 
+)
+
+(map_bb <- ggplot()+
+    geom_sf(data= sf_bbreak_4326, fill= "skyblue3", color= "black") 
+)
+
+(map_cc <- ggplot()+
+    geom_sf(data= sf_ccourt_4326, fill= "skyblue3", color= "black") 
+)
+
+#Filter CSTARS data set to just those within the Franks Tract polygon
+weeds_franks <- cstars_format %>% 
+  st_filter(sf_franks_4326) 
+#n=106 (not samples which are fewer)
+
+#Filter CSTARS data set to just those within the Big Break polygon
+weeds_bbreak <- cstars_format %>% 
+  st_filter(sf_bbreak_4326) 
+#n=61 rows (not samples which are fewer)
+
+#Filter CSTARS data set to just those within the Clifton Court polygon
+weeds_ccourt <- cstars_format %>% 
+  st_filter(sf_ccourt_4326) 
+#n=0; no sampling in clifton court
+
+#create map showing Franks Tract SAV data points
+#this shape file excludes two field points in False River I'd like to include 
 (sav_map_ft_only <- ggplot()+
     #plot waterways base layer
     geom_sf(data= WW_Delta_4326, fill= "skyblue3", color= "black") +
     #plot SAV sampling points
-    geom_sf(data=cstars_franks, fill= "red", color= "black", shape= 22, size= 3.5)+
+    geom_sf(data=weeds_franks, fill= "red", color= "black", shape= 22, size= 3.5)+
     #set bounding box for site
    # coord_sf( 
     #  xlim =c(-121.677, -121.576),
@@ -166,15 +189,34 @@ cstars_franks <- cstars_format %>%
     theme_bw()+
     ggtitle("Franks Tract")
 )        
+
+#create map showing Big Break SAV data points
+(sav_map_bb_only <- ggplot()+
+    #plot waterways base layer
+    geom_sf(data= WW_Delta_4326, fill= "skyblue3", color= "black") +
+    #plot SAV sampling points
+    geom_sf(data=weeds_bbreak, fill= "red", color= "black", shape= 22, size= 3.5)+
+    #set bounding box for site
+    coord_sf( 
+      xlim =c(-121.740, -121.685),
+      ylim = c(38.031, 38.005)
+    )+
+    #coord_sf(
+     # xlim = c(bbox_fr$xmin, bbox_fr$xmax),
+      #ylim = c(bbox_fr$ymin, bbox_fr$ymax)
+    #) + 
+    theme_bw()+
+    ggtitle("Big Break")
+)        
           
 #look at Franks Tract samples-------
 
 #date range
-unique(cstars_franks$date)
+unique(weeds_franks$date)
 #"2021-08-17" "2021-07-20"
 
 #number of samples
-ft_count<-cstars_franks %>% 
+ft_count<-weeds_franks %>% 
   st_set_geometry(NULL) %>%  #removes geometry
   distinct(latitude_wgs84,longitude_wgs84,date) %>% 
   summarize(count = n())
@@ -182,21 +224,47 @@ ft_count<-cstars_franks %>%
 #will be a few more once I get the larger Franks Tract polygon
 
 #how many open water samples?
-wat <- cstars_franks %>% 
+ft_wat <- weeds_franks %>% 
   filter(rake_teeth_corr==0 & is.na(species)) %>% 
   st_set_geometry(NULL) %>%  #removes geometry
   distinct(latitude_wgs84,longitude_wgs84,date,time)
 #n=5 open water samples
 
 #just look at samples where SAV was present and species have non-zero rake_prop
-franks_filter <- cstars_franks %>% 
+franks_filter <- weeds_franks %>% 
   filter(rake_teeth_corr>0 & !is.na(species) & (!is.na(rake_prop) & rake_prop!=0) )
 
 #which species
-unique(cstars_franks$species)
+unique(weeds_franks$species)
 #9 spp + algae + NA + unidentified
 
+#look at Big Break samples-------
 
+#date range
+unique(weeds_bbreak$date)
+#"2021-07-22"
+
+#number of samples
+bb_count<-weeds_bbreak %>% 
+  st_set_geometry(NULL) %>%  #removes geometry
+  distinct(latitude_wgs84,longitude_wgs84) %>% 
+  summarize(count = n())
+#30 samples
+
+#how many open water samples?
+bb_wat <- weeds_bbreak %>% 
+  filter(rake_teeth_corr==0 & is.na(species)) %>% 
+  st_set_geometry(NULL) %>%  #removes geometry
+  distinct(latitude_wgs84,longitude_wgs84)
+#n=2 open water samples
+
+#just look at samples where SAV was present and species have non-zero rake_prop
+bb_filter <- weeds_bbreak %>% 
+  filter(rake_teeth_corr>0 & !is.na(species) & (!is.na(rake_prop) & rake_prop!=0) )
+
+#which species
+unique(weeds_bbreak$species)
+#10 spp + NA + unidentified
 
 
 
