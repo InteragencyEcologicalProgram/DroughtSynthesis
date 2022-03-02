97131e2c559a1a2c387e4a9bc52d088fd166fbc9
Rasmussen <Nicholas.Rasmussen@water.ca.gov>
2022-03-02 11:28:06 -0800

    started script for permanova analysis of sepro sav data

---
+++ EDB/sepro_sav_permanova.R
@@ -0,0 +1,14 @@
+#Emergency Drought Barrier
+#SePro SAV surveys
+#conduct PERMANOVA analysis on SAV community
+#look for differences among years
+
+#load packages
+library(tidyverse)
+library(vegan)
+
+#read in data - veg, wq, herbicide use
+
+#format data set as matrix
+
+#
