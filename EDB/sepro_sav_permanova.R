aadf0c9cb2cd360848f2baf9bbabd68e0a6cb17a
Rasmussen <Nicholas.Rasmussen@water.ca.gov>
2022-03-02 16:51:31 -0800

    ran preliminary PERMANOVA

--- EDB/sepro_sav_permanova.R
+++ EDB/sepro_sav_permanova.R
@@ -15,15 +15,48 @@ sav <- read_csv("https://raw.githubusercontent.com/InteragencyEcologicalProgram/
 
 #PERMANOVA assumes roughly balances design
 #if looking at effect of year, then make sure years have similar sample sizes
-#I know they're generally 100 rakes per year but vary 50-200
 sav_n <- sav %>% 
   distinct(station,date) %>% 
   group_by(date) %>% 
   summarize(n = n())
 #all years have 100 samples except for two 
 #2016 = 45, 2015 = 200
-#should randomly subset 2015
+#should randomly subset 2015 to 100 samples
 #could exclude 2016 or just let the design be a bit unbalanced
+
+#create random subset of 100 rake samples from the 200 collected in 2015
+
+#start by grabbing the 2015 sampling data
+sav15 <- sav %>% 
+  #filter data set to just 2015 samples
+  filter(date=="2015-10-13") 
+
+#choose which stations to keep using random numbers
+sav15r <- sav15 %>% 
+  #condense to just the list of unique stations
+  distinct(station) %>% 
+  #add column of randomly generated numbers
+  mutate(random = sample(200, size = nrow(.), replace = F)) %>% 
+  #just keep rows assigned to 1-100
+  filter(random >0 & random < 101)
+
+#subset the 2015 data based on random numbers
+sav15sb <- left_join(sav15r,sav15) %>% 
+  select(-random)
+
+#remove the original full set of 2015 data from main data set
+sav_no15 <- sav %>% 
+  filter(date!="2015-10-13")
+
+#add the subsetted 2015 data to main data set
+sav_sub <- bind_rows(sav_no15,sav15sb)
+
+#make sure there are now 100 instead of 200 samples for 2015
+sav_n2 <- sav_sub %>% 
+  distinct(station,date) %>% 
+  group_by(date) %>% 
+  summarize(n = n())
+#looks like it worked
 
 #look at distribution of scores by species using histograms
 ggplot(sav, aes(x = rake_coverage_ordinal) )+
@@ -36,8 +69,8 @@ sav_spp_sum <- sav %>%
   #drop the visual samples and any abundances of zero
   filter(survey_method=="rake_weighted" & rake_coverage_ordinal!=0) %>% 
   group_by(species) %>% 
-  summarize(freq = n())  
-  filter(freq < 10)
+  summarize(freq = n()) %>% 
+  arrange(freq)
 #let's drop the spp with fewer than ten detections
 unique(sav_spp_sum$species)
 #five rare taxa removed in code below
@@ -46,11 +79,16 @@ unique(sav_spp_sum$species)
 sav_wide <- sav %>% 
   #add year column
   mutate(year = as.factor(year(sav$date))) %>% 
-  #remove the small number of visual only samples
-  filter(survey_method!="visual") %>%
+  filter(
+    #remove the small number of visual only samples
+    survey_method!="visual"
+    #remove rake samples with no SAV; can't have these in analysis
+    & sav_incidence==1
+    ) %>%
   #remove rare taxa (fewer than 10 detections ever)
   filter(species!="Heteranthera_dubia" & species!="Nitella_sp" & species!="Potamogeton_nodosus"      
          & species!="Potamogeton_pusillus"  & species!= "Potamogeton_zosteriformis") %>% 
+  #filter(species=="Potamogeton_richardsonii" | species== "Ceratophyllum_demersum" | species== "Egeria_densa" | species== "Najas_guadalupensis") %>% 
   #convert long to wide
   pivot_wider(id_cols=c(station,year,date)
               , names_from = species
@@ -70,11 +108,11 @@ sav_data<-sav_wide %>%
 #is there a more appropriate transformation to use for ordinal data?
 #also does the function do this automatically? check documentation
 sav_data_sqrt<-sqrt(sav_data)
-range(sav_data_sqrt) #0.000000 2.236068
+#range(sav_data_sqrt) #0.000000 2.236068
 #makes sense because raw scores are 0 to 5
 
 #PERMANOVA using adonis function
-#this currently isn't working. need to figure out why
+#decide if bray is the best method; I know I've also used horn
 permanova_sav<-adonis2(sav_data_sqrt ~ year, data=sav_env
                        #set number of permutations
                        ,permutations=999
@@ -82,6 +120,18 @@ permanova_sav<-adonis2(sav_data_sqrt ~ year, data=sav_env
                        #,by = "margin"
                        ,method="bray"
                        )
-permanova_sav
+permanova_sav #year is significant p < 0.001
 
+#plot the results
 
+#perform analysis of multivariate homogeneity of group dispersions
+#to determine if the multivariate scatter is similar among years
+#this is an assumption of PERMANOVA
+#if not similar among years, this can results in misleading significant p-values
+sav_dist <- vegdist(sav_data_sqrt, method="bray")
+dispersion <- betadisper(sav_dist, group=sav_env$year)
+permutest(dispersion) 
+#p<0.001, so we failed the test (ie, dispersions are not homogeneous)
+#so we don't know if centroids or dispersions caused the significant year effect
+
+plot(dispersion, hull=FALSE, ellipse=TRUE) ##sd ellipse
